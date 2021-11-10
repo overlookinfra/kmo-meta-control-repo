38c0bd0e50385b085c087c7bf417169ff010a84a
Jain Waldrip <jain.waldrip@puppet.com>
2021-11-09 18:20:08 -0600

    Add base profile

---
+++ site-modules/profile/manifests/base.pp
@@ -0,0 +1,13 @@
+# site-modules/profile/manifests/base.pp
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::base
+class profile::base {
+  class { 'motd':
+      content => "Hello! You are in ${trusted['extensions']['pp_datacenter']}"
+  }
+}
+
